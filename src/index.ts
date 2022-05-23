62b61cf9c710c4d0f6b6f98f1d4aa231717909a8
irishkooy <marines.qvcmarin@gmail.com>
2022-05-23 23:02:15 +0900

    huskey

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import World from './world'
 
 const root = document.getElementById('root')
-const world = new World('Hello Worldddddd!')
+const world = new World('Hello World!')
 world.sayHello(root)
+console.log('world')
+
+// 03.基本の型定義
+// import { primitiveSample } from './basic'
+
+// primitiveSample()
